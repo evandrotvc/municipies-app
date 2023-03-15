7bff6814cc13e4ba4f4483c222a8ad6375c1c43c
evandrotvc <evandro-tvc@hotmail.com>
2023-03-15 14:48:52 -0300

    feat: adjusts in database.yml and rubocop configs

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +13,10 @@ module MunicipiesApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # I18n config
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+    config.i18n.default_locale = :'pt-BR'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +24,8 @@ module MunicipiesApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths += %W["#{config.root}/app/validators/"]
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
