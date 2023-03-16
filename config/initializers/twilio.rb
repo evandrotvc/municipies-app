cd459824e175adb20fb21b4c9b3ce25b69cccbe9
evandrotvc <evandro-tvc@hotmail.com>
2023-03-16 13:30:12 -0300

    feat: config twilio initializer

---
+++ config/initializers/twilio.rb
@@ -0,0 +1,8 @@
+# # frozen_string_literal: true
+require 'twilio-ruby'
+
+
+Twilio.configure do |config|
+    config.account_sid = ENV.fetch('TWILIO_ACCOUNT_SID')
+    config.auth_token = ENV.fetch('TWILIO_AUTH_TOKEN')
+end
